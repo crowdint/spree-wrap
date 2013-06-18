84b6e4ad121987fdbab8741c0e5581d37b3d761f
David Padilla <david@crowdint.com>
2013-06-18 00:50:43 -0500

    Document the Spree::API::Product module

--- motion/spree/api/product.rb
+++ motion/spree/api/product.rb
@@ -5,7 +5,22 @@ module Spree
         base.send(:extend, ClassMethods)
       end
 
+      #
+      # This module defines all the API calls related to Products.
+      # All results return Spree::Product objects.
+      #
+      # API Reference
+      #
+      # http://api.spreecommerce.com/v1/products/#list-products
+      #
       module ClassMethods
+        #
+        # List products visible to the authenticated user.
+        #
+        #   Spree.products do |products|
+        #     # ..
+        #   end
+        #
         def products(&block)
           BW::HTTP.get(Spree.products_uri) do |response|
             json = BW::JSON.parse(response.body.to_str)
@@ -17,9 +32,24 @@ module Spree
           end
         end
 
+        #
+        # To view the details for a single product, make a request using that product's permalink:
+        #
+        #   Spree.product("a-product") do |product|
+        #     # ...
+        #   end
+        #
+        # You may also query by the product's id attribute:
+        #
+        #   Spree.product("id") do |product|
+        #     # ...
+        #   end
+        #
+        # Note that the API will attempt a permalink lookup before an ID lookup.
+        #
         def product(id, &block)
           BW::HTTP.get(Spree.product_uri(id)) do |response|
-            json = BW::JSON.parse(response.body.to_str)
+            json    = BW::JSON.parse(response.body.to_str)
             product = Spree::Product.new(json)
 
             block.call product
