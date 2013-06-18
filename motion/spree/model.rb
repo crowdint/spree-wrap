90e1b301ad6b6fe8e02424b237c2f593f36ec3cc
David Padilla <david@crowdint.com>
2013-06-18 00:52:20 -0500

    Document the Spree::Model module

--- motion/spree/model.rb
+++ motion/spree/model.rb
@@ -1,10 +1,16 @@
 module Spree
+  #
+  # This module defines common behavior for all the Models
+  #
   module Model
     def self.included(base)
       base.send(:extend, ClassMethods)
     end
 
     module ClassMethods
+      #
+      # Define the properties or attributes of the model.
+      #
       def define_model_attributes(*attrs)
         define_method :attributes do
           attrs
@@ -16,6 +22,9 @@ module Spree
       end
     end
 
+    #
+    # The initializer receives a hash with the attributes
+    #
     def initialize(attributes = {})
       self.attributes.each do |attr|
         send("#{attr}=", attributes[attr.to_s])
