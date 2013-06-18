1b9f4f5e141ee344343d3b1c6575365681409454
David Padilla <david@crowdint.com>
2013-06-18 00:53:23 -0500

    Document the Spree module

--- motion/spree.rb
+++ motion/spree.rb
@@ -1,8 +1,14 @@
 module Spree
+  #
+  # Getter for the endpoint URL
+  #
   def self.endpoint
     @endpoint
   end
 
+  #
+  # Setter for the endpoint URL
+  #
   def self.endpoint=(uri)
     @endpoint = uri
   end
