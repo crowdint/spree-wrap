55ab75d390fdb2270d43abafd993a181fd995035
David Padilla <david@crowdint.com>
2013-06-18 09:46:27 -0500

    Abstrouct out specs from Spree::API::Product and move them as specs for Spree::API::Query

--- spec/spree/api/product_spec.rb
+++ spec/spree/api/product_spec.rb
@@ -12,48 +12,4 @@ describe Spree::API::Product do
       Spree.product_uri(1).should == Spree.products_uri + "/1"
     end
   end
-
-  describe ".products" do
-    before do
-      disable_network_access!
-      stub_request(:get, Spree.products_uri).
-          to_return(json: load_webstub_response("products.json"))
-
-      @products = nil
-      Spree.products do |p|
-        @products = p
-        resume
-      end
-
-      wait_max 1 do
-        @products
-      end
-    end
-
-    it "returns an array of Spree::Product objects" do
-      @products.all? { |p| p.is_a?(Spree::Product) }.should == true
-    end
-  end
-
-  describe ".product" do
-    before do
-      disable_network_access!
-      stub_request(:get, Spree.product_uri(1)).
-          to_return(json: load_webstub_response("product.1.json"))
-
-      @product = nil
-      Spree.product(1) do |p|
-        @product = p
-        resume
-      end
-
-      wait_max 1 do
-        @product
-      end
-    end
-
-    it "returns a Spree::Product object" do
-      @product.is_a?(Spree::Product).should == true
-    end
-  end
 end
---
+++ spec/spree/api/query_spec.rb
@@ -0,0 +1,69 @@
+module Spree
+  class Foo
+    include Spree::Model
+
+    define_model_attributes :id, :name
+  end
+
+  module API
+    class Foo
+      extend Spree::API::Query
+    end
+  end
+end
+
+describe Spree::API::Query do
+  extend WebStub::SpecHelpers
+
+  before do
+    disable_network_access!
+  end
+
+  describe "#collection_query" do
+    before do
+      @uri = "http://example.com/api/foos"
+      @data = { foos: [{ id: "1", name: "Foo 1" }, { id: "2", name: "Foo 2" }] }
+      stub_request(:get, @uri).
+          to_return(json: @data)
+
+      Spree::API::Foo.collection_query("foos", Spree::Foo, @uri) do |f|
+        @foos = f
+        resume
+      end
+
+      # Wait for the BW::HTTP Thread
+      wait_max 1 { @foos }
+    end
+
+    it "maps received data to objects of the specified class" do
+      @foos.each_with_index do |f, i|
+        f.is_a?(Spree::Foo).should.be.true
+        f.id.should.equal   @data[:foos][i][:id]
+        f.name.should.equal @data[:foos][i][:name]
+      end
+    end
+  end
+
+  describe "#object_query" do
+    before do
+      @uri = "http://example.com/api/foos/1"
+      @data = { id: "1", name: "Foo 1" }
+      stub_request(:get, @uri).
+          to_return(json: @data)
+
+      Spree::API::Foo.object_query(Spree::Foo, @uri) do |f|
+        @foo = f
+        resume
+      end
+
+      # Wait for the BW::HTTP Thread
+      wait_max 1 { @foo }
+    end
+
+    it "maps received data to an object of the specified class" do
+      @foo.is_a?(Spree::Foo).should.be.true
+      @foo.id.should.equal   @data[:id]
+      @foo.name.should.equal @data[:name]
+    end
+  end
+end
