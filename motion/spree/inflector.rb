be81c3fce1597ba102f71abc7c4fcc8e9e686f94
David Padilla <david@crowdint.com>
2013-06-20 13:49:15 -0500

    The evolution of Spree::Inflector

--- motion/spree/inflector.rb
+++ motion/spree/inflector.rb
@@ -1,14 +1,33 @@
 module Spree
   #
-  # This class should grow as needed to emulate ActiveSupport Inflections.
-  #
-  # Also, placeholder for other related helper methods.
-  #
-  # For now this is all we need.
+  # Handles the pluralization of resource names for the API classes
   #
   class Inflector
-    def self.pluralize(singular)
-      (singular == "country") ? "countries" : "#{singular}s"
+    def self.inflections
+      @inflections ||= []
     end
+
+    def self.inflections=(value)
+      @inflections = value
+    end
+
+    def self.pluralize(singular)
+      inflection = self.inflections.detect { |i| i[:singular] == singular }
+      raise UndefinedInflection unless inflection
+      inflection[:plural]
+    end
+
+    def self.inflection(singular, plural)
+      inflections << Inflection.new(singular, plural)
+    end
+
+    Inflection = Struct.new(:singular, :plural)
+
+    class UndefinedInflection < Exception ; end
   end
 end
+
+Spree::Inflector.inflection 'cat', 'cats'
+Spree::Inflector.inflection 'country', 'countries'
+Spree::Inflector.inflection 'product', 'products'
+Spree::Inflector.inflection 'variant', 'variants'
--- spec/spree/inflector_spec.rb
+++ spec/spree/inflector_spec.rb
@@ -1,11 +1,28 @@
 describe Spree::Inflector do
   describe ".pluralize" do
-    it "adds an s to the end of the specified singular" do
-      Spree::Inflector.pluralize("cat").should == "cats"
+    describe "defined Inflection" do
+      it "adds an s to the end of the specified singular" do
+        Spree::Inflector.pluralize("cat").should == "cats"
+      end
+
+      it "pluralizes country" do
+        Spree::Inflector.pluralize("country").should == "countries"
+      end
     end
 
-    it "pluralizes country" do
-      Spree::Inflector.pluralize("country").should == "countries"
+    describe "undefined Inflection" do
+      it "raises Spree::Inflector::UndefinedInflection" do
+        lambda {
+          Spree::Inflector.pluralize("42")
+        }.should.raise(Spree::Inflector::UndefinedInflection)
+      end
+    end
+  end
+
+  describe ".inflections" do
+    it "defines a new inflection" do
+      Spree::Inflector.inflection 'foo', 'bar'
+      Spree::Inflector.pluralize('foo').should == 'bar'
     end
   end
 end
