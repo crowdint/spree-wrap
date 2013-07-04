ad5655f47cdeca010e036a4c2e0fa15661139b70
David Padilla <david@crowdint.com>
2013-07-03 23:53:12 -0500

    Add X-Spree-Token header to all requests

--- motion/spree/api/query.rb
+++ motion/spree/api/query.rb
@@ -3,7 +3,7 @@ module Spree
     module Query
       def collection_query(collection_name, object_class, uri, &block)
         if block_given?
-          BW::HTTP.get(uri) do |response|
+          BW::HTTP.get(uri, default_query_headers) do |response|
             json       = BW::JSON.parse(response.body.to_str)
             collection = (json[collection_name] || []).map do |object_json|
               object_class.new(object_json)
@@ -18,7 +18,7 @@ module Spree
 
       def object_query(object_class, uri, &block)
         if block_given?
-          BW::HTTP.get(uri) do |response|
+          BW::HTTP.get(uri, default_query_headers) do |response|
             json   = BW::JSON.parse(response.body.to_str)
             object = object_class.new(json)
 
@@ -27,6 +27,11 @@ module Spree
         else
           BW::HTTP.get(uri)
         end
+      end
+
+      private
+      def default_query_headers
+        { headers: { "X-Spree-Token" => Spree.token }}
       end
     end
   end
