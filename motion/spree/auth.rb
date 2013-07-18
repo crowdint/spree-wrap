1359972901d3f12a41bd7e7982a16fe5a9bf9f6c
Sergio Morales <sergio.morales@crowdint.com>
2013-07-18 15:39:14 -0500

    First prototype of the Auth class for authenticating users

---
+++ motion/spree/auth.rb
@@ -0,0 +1,81 @@
+module Spree
+  class << self
+    attr_accessor :authentication_endpoint,
+                  :registration_endpoint,
+                  :cookie,
+                  :order_token
+  end
+
+  class Auth
+    class << self
+      attr_reader :current_user
+    end
+
+
+    def self.authenticate!(user, &b)
+      @current_user = user
+
+      BW::HTTP.post(Spree.authentication_endpoint, authentication_request) do |response|
+        if response.ok?
+          store_current_user(response)
+        else
+          @current_user = nil
+        end
+        b.call(response)
+      end
+    end
+
+    def self.register!(user, &b)
+      @current_user = user
+
+      BW::HTTP.post(Spree.registration_endpoint, registration_request) do |response|
+        if response.ok?
+          store_current_user(response)
+        else
+          @current_user = nil
+        end
+        b.call(response)
+      end
+    end
+
+    def self.authentication_request
+      { headers: default_headers, payload: credentials }
+    end
+
+    def self.registration_request
+      { headers: default_headers, payload: new_user_data }
+    end
+
+    def self.default_headers
+      @default_headers ||= {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      }
+    end
+
+    def self.credentials
+      BW::JSON.generate({
+        spree_user: {
+          email: current_user.email,
+          password: current_user.password
+        }
+      })
+    end
+
+    def self.new_user_data
+      BW::JSON.generate({
+        spree_user: {
+          email: current_user.email,
+          password: current_user.password,
+          password_confirmation: current_user.password_confirmation
+        }
+      })
+    end
+
+    def self.store_current_user(response)
+      Spree.token   = current_user.spree_api_key
+      Spree.cookie  = response.headers["Set-Cookie"]
+    end
+
+  end
+end
