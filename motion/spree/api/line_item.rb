74f269c43eb0af77010184c5f3f4ffba318f3c58
Sergio Morales <sergio.morales@crowdint.com>
2013-08-21 16:40:37 -0500

    Spree.update_line_item and Spree.delete_line_item, check

--- motion/spree/api/line_item.rb
+++ motion/spree/api/line_item.rb
@@ -30,6 +30,30 @@ module Spree
         end
       end
 
+      def update_line_item(attributes, &block)
+        if block_given?
+          BW::HTTP.put(line_item_uri(attributes[:id]), create_line_item_request(attributes)) do |response|
+            if response.ok?
+              json = BW::JSON.parse response.body
+              line_item = Spree::LineItem.new(json)
+            else
+              line_item = nil
+            end
+
+            block.call(line_item, response)
+          end
+        else
+          BW::HTTP.post(line_items_uri, create_line_item_request(attributes))
+        end
+      end
+
+      def delete_line_item(id, &block)
+        block_present = block_given?
+        BW::HTTP.delete(line_item_uri(id), {headers: default_headers}) do |response|
+          block.call(response) if block_present
+        end
+      end
+
       private
 
       def create_line_item_request(options)
@@ -40,12 +64,16 @@ module Spree
               quantity:   (options[:quantity] || 1)
             }
           }),
-          headers: {
-            "Accept"        => "application/json",
-            "Content-Type"  => "application/json",
-            "Cookie"        => Spree.cookie,
-            "X-Spree-Token" => Spree.token
-          }
+          headers: default_headers
+        }
+      end
+
+      def default_headers
+        {
+          "Accept"        => "application/json",
+          "Content-Type"  => "application/json",
+          "Cookie"        => Spree.cookie,
+          "X-Spree-Token" => Spree.token
         }
       end
     end
